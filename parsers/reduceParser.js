b34b01494eb13a81c0455012e0297e43fe57bc25
Bohdan Meltiukhov <bohdan-meltiukhov@teamdev.com>
2020-03-17 19:32:44 +0200

    Add module approach to parsing

---
+++ parsers/reduceParser.js
@@ -0,0 +1,35 @@
+import Parser from "./parser.js";
+
+/**
+ * A parser that uses the javascript reduce method to turn an input string to an array of list items.
+ */
+export default class ReduceParser extends Parser{
+
+    /**
+     * Turns the provided string to an array of list items with the help of the javascript reduce method.
+     *
+     * @param {string} input - The string to be parsed.
+     * @returns {HTMLLIElement[]} List items to be appended to the output list.
+     */
+    parse(input) {
+        let lines = input.split('\n');
+
+        lines = lines.reduce(function(modifiedLines, line) {
+            const modifiedLine = line.split(',')
+                .map((value) => (value !== '') ? value : '<empty>')
+                .join(' ');
+
+            modifiedLines.push(modifiedLine);
+            return modifiedLines;
+        }, []);
+
+        const listItems = lines.reduce(function(items, line) {
+            const listItem = document.createElement('li');
+            listItem.innerText = line;
+            items.push(listItem);
+            return items;
+        }, []);
+
+        return listItems;
+    }
+}
