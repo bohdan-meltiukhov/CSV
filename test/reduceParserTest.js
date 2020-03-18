2e7eaa07db87242708cdce16c5816ed9fd81a12c
Bohdan Meltiukhov <bohdan-meltiukhov@teamdev.com>
2020-03-18 12:59:53 +0200

    Add promises to tests

--- test/reduceParserTest.js
+++ test/reduceParserTest.js
@@ -2,26 +2,34 @@ import ReduceParser from "../parsers/reduceParser.js";
 
 QUnit.module('ReduceParser test');
 
-QUnit.test('should create list items with correct inner text.', function(assert) {
+QUnit.test('should create list items with correct inner text.', async function(assert) {
     const parser = new ReduceParser();
 
-    assert.strictEqual(parser.parse('')[0].innerText, '<empty>',
+    let result = await parser.parse('');
+
+    assert.strictEqual(result[0].innerText, '<empty>',
         'An empty string should create an <empty> list item.');
 
-    assert.strictEqual(parser.parse('1,2,3')[0].innerText, '1 2 3',
+    result = await parser.parse('1,2,3');
+
+    assert.strictEqual(result[0].innerText, '1 2 3',
         'A single line of values should become separated by space.');
 
-    const listItems = parser.parse('1,2,3\n4,5');
+    result = await parser.parse('1,2,3\n4,5');
 
-    assert.strictEqual(listItems[0].innerText, '1 2 3',
+    assert.strictEqual(result[0].innerText, '1 2 3',
         'The first line of multiple lines should be parsed correctly.');
 
-    assert.strictEqual(listItems[1].innerText, '4 5',
+    assert.strictEqual(result[1].innerText, '4 5',
         'The second line of multiple lines should be parsed correctly.');
 
-    assert.strictEqual(parser.parse('1,,5')[0].innerText, '1 <empty> 5',
+    result = await parser.parse('1,,5');
+
+    assert.strictEqual(result[0].innerText, '1 <empty> 5',
         'A skipped value should be displayed as <empty>.');
 
-    assert.strictEqual(parser.parse('\n1,2,3')[0].innerText, '<empty>',
+    result = await parser.parse('\n1,2,3');
+
+    assert.strictEqual(result[0].innerText, '<empty>',
         'A skipped line should be displayed as <empty>.');
 });
